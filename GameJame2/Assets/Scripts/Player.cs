3c8c59eb7499230f1b2f44c7043c8e7a6ee6e414
isaac1441 <issaac1414@gmail.com>
2024-07-14 03:27:29 -0500

    added poop

---
+++ GameJame2/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage){
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
