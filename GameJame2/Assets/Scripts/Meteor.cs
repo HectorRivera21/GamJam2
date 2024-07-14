c9e9338ecf4fb3e5626209926bec84cd3c735fd3
Hector <redmagix2000@gmail.com>
2024-07-14 11:21:17 -0500

    added score and local score saving feature

--- GameJame2/Assets/Scripts/Meteor.cs
+++ GameJame2/Assets/Scripts/Meteor.cs
@@ -6,21 +6,29 @@ public class Meteor : MonoBehaviour
 {
     [SerializeField] private GameObject target;
     [SerializeField] private float speed = 8.5f;
+    private int scoreValue = 100;
     public int meteor_health = 100;
 
-    
-    void Start()
+    void TakeDamage(int damage)
     {
-        //do nothin
+        meteor_health -= damage;
+        if(meteor_health <= 0)
+        {
+            DestroyMeteor();
+        }
     }
 
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
     }
-    
+    void DestroyMeteor()
+    {
+        GameManager.Instance.AddScore(scoreValue);
+        Destroy(gameObject);
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(gameObject);
+        DestroyMeteor();
     }
 }
